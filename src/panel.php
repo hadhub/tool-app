39e12fa5223468a642756c2ec4851e8454c8cbea
had <85348372+hadhub@users.noreply.github.com>
2024-08-06 11:07:25 +0200

    Update panel.php

--- src/panel.php
+++ src/panel.php
@@ -4,17 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/assets/css/default.css">
+    <script src="../assets/js/todo.js"></script>
     <title>My Dashboard</title>
 </head>
 <body>
-    <script src="../assets/js/todo.js"></script>
     <h2>Welcome to ur dashboard</h2>
 
-    <form action="" method="post">
+    <form action="" method="POST">
         <p>enter the name of ur todo list : </p>
-        <input type="todo list" name="todo_name">
-        <button type="submit" name="test_button" onclick="myFunction()" >Create ur todo list</button>
+        <input type="text" name="todo_name" id="custom_todo">
+        <button type="submit" name="create_button" onclick="createTodoList()">Create your to-do list</button>
+        <button type="submit" name="clear_button" onclick="clearTodoList()">Clear your to-do list</button>
     </form>
-
+    <div class="container">
+        <p id="output_name"></p>
+    </div>
 </body>
 </html>
