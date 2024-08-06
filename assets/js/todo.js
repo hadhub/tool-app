dcc209d82e4a2106d113bff4b052c2af95e56165
had <85348372+hadhub@users.noreply.github.com>
2024-08-06 14:32:10 +0200

    Update todo.js

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,3 +1,12 @@
-function myFunction() {
-    prompt("ça marche !")
+function createTodoList() {
+    let todo_list_name = document.getElementById('custom_todo').value;
+    let output = document.getElementById('output_name');
+    console.log(output);
+    console.log(todo_list_name);
+
+    output.innerHTML = todo_list_name;
+}
+
+function clearTodoList() {
+    console.log("WIP");
 }
